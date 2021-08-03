9610361693c97d40db30cd4bfb38b9846d4263f2
cauaFurquim <cauafurquim012@gmail.com>
2021-08-03 11:04:33 -0300

    Aula dia 03/08

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const servidor = express()
+
+servidor.get(
+    '/', (req, res, next) => {
+        return res.send({
+            'mensagem' : 'API em execução'
+        }
+        )
+    }
+)
+
+servidor.listen(3000, () => {
+    console.log('Servidor funcionando')
+})
